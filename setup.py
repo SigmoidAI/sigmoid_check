71c21009a5754d6c7c5790afaad6808261989dcd
pyramixofficial <balamatiuc2@gmail.com>
2024-04-30 15:25:36 +0300

    update version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md") as f:
 
 setup(
     name="sigmoid_check",
-    version="0.0.4",
+    version="0.0.5",
     packages=find_packages(),
     description="A package for checking the implementation of tasks in Sigmoid Courses",
     long_description=long_description,
