888b3b0a9ba380f254907d2295b474e845c683f2
pyramixofficial <balamatiuc2@gmail.com>
2024-08-25 16:01:28 +0300

    update version of package

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md") as f:
 
 setup(
     name="sigmoid_check",
-    version="0.0.9",
+    version="0.0.10",
     packages=find_packages(),
     description="A package for checking the implementation of tasks in Sigmoid Courses",
     long_description=long_description,
