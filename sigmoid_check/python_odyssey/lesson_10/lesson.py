f3ae710ef490d22f309d01943e7a0d3bf6e2c4e0
Ank <ank215900@gmail.com>
2024-09-06 13:57:18 +0300

    updated lesson 10 tox and add .idea to gitignore

--- sigmoid_check/python_odyssey/lesson_10/lesson.py
+++ sigmoid_check/python_odyssey/lesson_10/lesson.py
@@ -107,12 +107,13 @@ class Lesson10:
             return f"Exercise 6: Error - {e}"
 
     def check_task_7(self, func):
-        """Task: Creați o funcție cu numele "task_7" care utilizând list comprehension va returna un set cu multiplii de 3 de la 1 la n, unde n este un argument al funcției.
+        """Task: Creați o funcție cu numele "task_7" care utilizând list comprehension va returna un set cu multiplii de
+        3 de la 1 la n, unde n este un argument al funcției.
         Funcția va primi ca argument un număr n care va reprezenta numărul până la care se va face maparea.
-        Exemplu: Pentru n=50 rezultatul returnat va fi {3, 6, 9, 12, 15, 18, 21, 24, 27, 30, 33, 36, 39, 42, 45, 48}       
+        Exemplu: Pentru n=50 rezultatul returnat va fi {3, 6, 9, 12, 15, 18, 21, 24, 27, 30, 33, 36, 39, 42, 45, 48}
         """
         n = 50
-        expected_output = {i for i in range(1, n+1) if i % 3 == 0}
+        expected_output = {i for i in range(1, n + 1) if i % 3 == 0}
         try:
             student_output = func(n)
             if student_output == expected_output:
@@ -125,7 +126,8 @@ class Lesson10:
             return f"Exercise 7: Error - {e}"
 
     def check_task_8(self, func):
-        """Task: Creați o funcție cu numele "task_8" care are ca argument o listă de numere și va returna media aritmetică a numerelor din listă.
+        """Task: Creați o funcție cu numele "task_8" care are ca argument o listă de numere și va returna media
+        aritmetică a numerelor din listă.
         Exemplu: Pentru lista [1, 2, 3, 4, 5] rezultatul va fi 3.0
         """
         numbers = [1, 2, 3, 4, 5]
@@ -140,9 +142,10 @@ class Lesson10:
                 return "Exercise 1: Incorrect. Please try again."
         except Exception as e:
             return f"Exercise 8: Error - {e}"
-        
+
     def check_task_9(self, func):
-        """Task: Creați o funcție cu numele "task_9" care are ca argument un număr și va returna `True` dacă numărul este par, altfel `False`.
+        """Task: Creați o funcție cu numele "task_9" care are ca argument un număr și va returna `True` dacă numărul
+        este par, altfel `False`.
         Exemplu: Pentru numărul 4 rezultatul va fi `True`, iar pentru numărul 5 rezultatul va fi `False`.
         """
         number_1 = 4
@@ -160,11 +163,13 @@ class Lesson10:
                 return "Exercise 1: Incorrect. Please try again."
         except Exception as e:
             return f"Exercise 9: Error - {e}"
-        
+
     def check_task_10(self, func):
-        """Task: Creați o funcție cu numele "task_10" care are ca argument numele și vârsta unei persoane ca argumente poziționale și orașul ca un argument opțional,
+        """Task: Creați o funcție cu numele "task_10" care are ca argument numele și vârsta unei persoane ca argumente
+        poziționale și orașul ca un argument opțional,
         apoi returnează o descriere a persoanei în forma "Nume: *nume*, Varsta: *varsta*, Oras: *oras*".
-        Exemplu: Pentru numele "Ana", vârsta 32 și orașul "București" rezultatul va fi "Nume: Ana, Varsta: 32, Oras: București"
+        Exemplu: Pentru numele "Ana", vârsta 32 și orașul "București" rezultatul va fi "Nume: Ana, Varsta: 32,
+        Oras: București"
         """
         name = "Ana"
         age = 32
@@ -180,9 +185,10 @@ class Lesson10:
                 return "Exercise 1: Incorrect. Please try again."
         except Exception as e:
             return f"Exercise 10: Error - {e}"
-        
+
     def check_task_11(self, func):
-        """Task: Creați o funcție cu numele "task_11" care acceptă o listă variabilă de numere și returnează valoarea maximă.
+        """Task: Creați o funcție cu numele "task_11" care acceptă o listă variabilă de numere și returnează valoarea
+        maximă.
         Exemplu: Pentru lista [10, 20, 30, 40, 50] rezultatul va fi 50
         """
         numbers = [10, 20, 30, 40, 50]
@@ -197,7 +203,7 @@ class Lesson10:
                 return "Exercise 1: Incorrect. Please try again."
         except Exception as e:
             return f"Exercise 11: Error - {e}"
-        
+
     def check_task_12(self, func):
         """Task: Creați o funcție cu numele "task_12" care acceptă un număr și returnează factorialul său.
         Exemplu: Pentru numărul 5 rezultatul va fi 120
@@ -214,7 +220,7 @@ class Lesson10:
                 return "Exercise 1: Incorrect. Please try again."
         except Exception as e:
             return f"Exercise 12: Error - {e}"
-        
+
     def check_task_13(self, func):
         """Task: Creați o funcție cu numele "task_13" care acceptă două numere și returnează suma și produsul lor.
         Exemplu: Pentru numerele 3 și 4 rezultatul va fi (7, 12)
@@ -232,9 +238,11 @@ class Lesson10:
                 return "Exercise 1: Incorrect. Please try again."
         except Exception as e:
             return f"Exercise 13: Error - {e}"
-        
+
     def check_task_14(self, func):
-        """Task: Creați o funcție cu numele "task_14" care acceptă un număr ce reprezintă vârsta unei persoane și returnează textul "minor" dacă vârsta este sub 18 ani, "adult" dacă vârsta este între 18 și 65 ani și "senior" dacă vârsta este peste 65 de ani.
+        """Task: Creați o funcție cu numele "task_14" care acceptă un număr ce reprezintă vârsta unei persoane și
+        returnează textul "minor" dacă vârsta este sub 18 ani, "adult" dacă vârsta este între 18 și 65 ani și "senior"
+        dacă vârsta este peste 65 de ani.
         Exemplu: Pentru vârsta 32 rezultatul va fi "adult"
         """
         age_1 = 65
@@ -247,7 +255,8 @@ class Lesson10:
             student_output_1 = func(age_1)
             student_output_2 = func(age_2)
             student_output_3 = func(age_3)
-            if student_output_1 == expected_output_1 and student_output_2 == expected_output_2 and student_output_3 == expected_output_3:
+            if (student_output_1 == expected_output_1 and student_output_2 == expected_output_2
+                    and student_output_3 == expected_output_3):
                 self.status_tasks["task_14"] = True
                 return "Exercise 1: Correct! Well done."
             else:
@@ -255,9 +264,10 @@ class Lesson10:
                 return "Exercise 1: Incorrect. Please try again."
         except Exception as e:
             return f"Exercise 14: Error - {e}"
-        
+
     def check_task_15(self, func):
-        """Task: Creați o funcție cu numele "task_15" care acceptă un string și returnează `True` dacă string-ul este un palindrom, altfel `False`.
+        """Task: Creați o funcție cu numele "task_15" care acceptă un string și returnează `True` dacă string-ul este
+        un palindrom, altfel `False`.
         Exemplu: Pentru string-ul "ana" rezultatul va fi `True`, iar pentru string-ul "test" rezultatul va fi `False`.
         """
         string_1 = "ana"
@@ -275,9 +285,10 @@ class Lesson10:
                 return "Exercise 1: Incorrect. Please try again."
         except Exception as e:
             return f"Exercise 15: Error - {e}"
-        
+
     def check_task_16(self, func):
-        """Task: Creați o funcție cu numele "task_16" care acceptă un string și returnează același string cu literele inversate.
+        """Task: Creați o funcție cu numele "task_16" care acceptă un string și returnează același string
+        cu literele inversate.
         Exemplu: Pentru string-ul "test" rezultatul va fi "tset"
         """
         string = "test"
@@ -292,9 +303,10 @@ class Lesson10:
                 return "Exercise 1: Incorrect. Please try again."
         except Exception as e:
             return f"Exercise 16: Error - {e}"
-        
+
     def check_task_17(self, func):
-        """Task: Creați o funcție cu numele "task_17" care acceptă un string și returnează numărul de cuvinte din string.
+        """Task: Creați o funcție cu numele "task_17" care acceptă un string și returnează numărul
+        de cuvinte din string.
         Exemplu: Pentru string-ul "Hello, World!" rezultatul va fi 2
         """
         string = "Hello, World!"
@@ -309,9 +321,10 @@ class Lesson10:
                 return "Exercise 1: Incorrect. Please try again."
         except Exception as e:
             return f"Exercise 17: Error - {e}"
-        
+
     def check_task_18(self, func):
-        """Task: Creați o funcție cu numele "task_18" care acceptă un număr ce reprezintă temperatura în grade Celsius și returnează temperatura în grade Fahrenheit.
+        """Task: Creați o funcție cu numele "task_18" care acceptă un număr ce reprezintă temperatura în grade Celsius
+        și returnează temperatura în grade Fahrenheit.
         Exemplu: Pentru temperatura 0 rezultatul va fi 32.0
         """
         celsius = 0
@@ -326,9 +339,10 @@ class Lesson10:
                 return "Exercise 1: Incorrect. Please try again."
         except Exception as e:
             return f"Exercise 18: Error - {e}"
-        
+
     def check_task_19(self, func):
-        """Task: Creați o funcție cu numele "task_19" care acceptă un număr și returnează `True` dacă numărul este prim, altfel `False`.
+        """Task: Creați o funcție cu numele "task_19" care acceptă un număr și returnează `True` dacă numărul este prim,
+         altfel `False`.
         Exemplu: Pentru numărul 7 rezultatul va fi `True`, iar pentru numărul 10 rezultatul va fi `False`.
         """
         number_1 = 7
@@ -346,9 +360,10 @@ class Lesson10:
                 return "Exercise 1: Incorrect. Please try again."
         except Exception as e:
             return f"Exercise 19: Error - {e}"
-        
+
     def check_task_20(self, func):
-        """Task: Creați o funcție cu numele "task_20" care acceptă un număr și returnează `True` dacă numărul este un număr perfect, altfel `False`.
+        """Task: Creați o funcție cu numele "task_20" care acceptă un număr și returnează `True` dacă numărul este un
+        număr perfect, altfel `False`.
         Un număr perfect este un număr întreg pozitiv care este egal cu suma divizorilor săi, excluzând numărul însuși.
         Exemplu: Pentru numărul 28 rezultatul va fi `True`, iar pentru numărul 10 rezultatul va fi `False`.
         """
@@ -367,9 +382,10 @@ class Lesson10:
                 return "Exercise 1: Incorrect. Please try again."
         except Exception as e:
             return f"Exercise 20: Error - {e}"
-        
+
     def check_task_21(self, func):
-        """Task: Creați o funcție cu numele "task_21" care acceptă un număr și returnează `True` dacă numărul este un număr Armstrong, altfel `False`.
+        """Task: Creați o funcție cu numele "task_21" care acceptă un număr și returnează `True` dacă numărul este un
+        număr Armstrong, altfel `False`.
         Un număr Armstrong este un număr care este egal cu suma puterilor sale de cifre.
         Exemplu: Pentru numărul 153 rezultatul va fi `True`, iar pentru numărul 10 rezultatul va fi `False`.
         """
@@ -388,9 +404,10 @@ class Lesson10:
                 return "Exercise 1: Incorrect. Please try again."
         except Exception as e:
             return f"Exercise 21: Error - {e}"
-        
+
     def check_task_22(self, func):
-        """Task: Creați o funcție cu numele "task_22" care acceptă un număr și returnează `True` dacă numărul este un număr Harshad, altfel `False`.
+        """Task: Creați o funcție cu numele "task_22" care acceptă un număr și returnează `True` dacă numărul este un
+        număr Harshad, altfel `False`.
         Un număr Harshad este un număr care este divizibil cu suma cifrelor sale.
         Exemplu: Pentru numărul 18 rezultatul va fi `True`, iar pentru numărul 14 rezultatul va fi `False`.
         """
@@ -409,9 +426,10 @@ class Lesson10:
                 return "Exercise 1: Incorrect. Please try again."
         except Exception as e:
             return f"Exercise 22: Error - {e}"
-        
+
     def check_task_23(self, func):
-        """Task: Creați o funcție cu numele "task_23" care primește ca argument un număr și returneaeză o listă cu primele n numere ale seriei Fibonacci.
+        """Task: Creați o funcție cu numele "task_23" care primește ca argument un număr și returneaeză o listă cu
+        primele n numere ale seriei Fibonacci.
         Exemplu: Pentru numărul 5 rezultatul va fi [0, 1, 1, 2, 3]
         """
         number = 5
@@ -426,9 +444,10 @@ class Lesson10:
                 return "Exercise 1: Incorrect. Please try again."
         except Exception as e:
             return f"Exercise 23: Error - {e}"
-        
+
     def check_task_24(self, func):
-        """Task: Creați o funcție cu numele "task_24" care primește ca argument un număr și returnează o listă cu divizorii numărului respectiv.
+        """Task: Creați o funcție cu numele "task_24" care primește ca argument un număr și returnează o listă cu
+        divizorii numărului respectiv.
         Exemplu: Pentru numărul 10 rezultatul va fi [1, 2, 5, 10]
         """
         number = 10
@@ -443,7 +462,6 @@ class Lesson10:
                 return "Exercise 1: Incorrect. Please try again."
         except Exception as e:
             return f"Exercise 24: Error - {e}"
-
 
     def get_completion_percentage(self):
         """Return the completion percentage of the tasks"""
